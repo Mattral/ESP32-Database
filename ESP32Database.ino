ba10f9d8b81c4c853a00f4bdad969614e44567eb
Mattral <88831350+Mattral@users.noreply.github.com>
2022-01-08 21:17:24 +0630

    Add files via upload

---
+++ ESP32Database.ino
@@ -0,0 +1,58 @@
+
+ #include <WiFi.h>
+ #include <HTTPClient.h>
+const char* ssid = " ";
+const char* password = " ";
+const char* serverName = " /post-data.php";
+String apiKeyValue = "";                        //fill
+String sensorName = "Touch";
+String sensorLocation = "HomeOffice";
+void setup() {
+ Serial.begin(115200);
+ 
+ WiFi.begin(ssid, password);
+ Serial.println("Connecting to the Wifi Network");
+ while(WiFi.status() != WL_CONNECTED) { 
+ delay(500);
+ Serial.print(".");
+ }
+ Serial.println("");
+ Serial.print("WiFi is Connected at this IP Address : ");
+ Serial.println(WiFi.localIP());
+}
+void loop() {
+ //Check WiFi connection status
+ if(WiFi.status()== WL_CONNECTED){
+ HTTPClient http;
+ 
+ http.begin(serverName);
+ 
+ http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+ 
+ // Prepare your HTTP POST request data
+ String httpRequestData = "api_key=" + apiKeyValue + "&sensor=" + 
+sensorName
+ + "&location=" + sensorLocation + "&value1=" + 
+touchRead(2)
+ + "&value2=" + touchRead(4) + "&value3=" + 
+touchRead(15) + "";
+ Serial.print("httpRequestData: ");
+ Serial.println(httpRequestData);
+ 
+ int httpResponseCode = http.POST(httpRequestData);
+ 
+ if (httpResponseCode>0) {
+ Serial.print("HTTP Response code: ");
+ Serial.println(httpResponseCode);
+ }
+ else {
+ Serial.print("Error code: ");
+ Serial.println(httpResponseCode);
+ }
+ http.end();
+ }
+ else {
+ Serial.println("WiFi Disconnected");
+ }
+ delay(30000); 
+}
